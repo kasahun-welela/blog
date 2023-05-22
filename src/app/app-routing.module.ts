9494df15b00c48e43fe0c9f775094a7bd413b60c
kasahun welela <getasewmequanint@cbe.com.et>
2023-05-22 17:50:10 +0300

    all components are created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contactus', component:ContactUsComponent},
+  {path:'news', component:NewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './nabvar/nabvar.component';
+import { CardComponent } from './card/card.component';
+import axios from 'axios';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NewsComponent } from './news/news.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    CardComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent,
+    NewsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
